dcdad20b439a592289e3bbf2fa73a07294c42ecc
Your Name <cola770>
2022-08-26 13:53:29 +0200

    onther commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -16,13 +17,18 @@ class QuestionController
     }
 
     /**
-     * @Route("questions/{slug}")
+     * @Route("question/{slug}")
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show a question "%s"!',
-            $slug
-        ));
+        $answers = [
+            'answer1',
+            'answer2',
+            'answer3',
+        ];
+        return $this->render('question/show.html.twig',[
+            'question' => ucwords(str_replace('-',' ',$slug)),
+            'answers' => $answers,
+        ]);
     }
 }
